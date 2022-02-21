8090411a2aaba9a922d986521256bab116c7a04a
JanLen06 <100159008+JanLen06@users.noreply.github.com>
2022-02-21 19:41:04 +0100

    Add files via upload
    These are the two files so far. Main is, as the name says, the main file to run, while lib is the library file supportiung main with the functions

---
+++ main.py
@@ -0,0 +1,37 @@
+import lib
+
+#Instructions
+print("Beachten:")
+print("beim Eingeben der Funktion werden")
+print("Luecken zwischen Exponent und dem")
+print("naechsten Vorzeichen gemacht!")
+print("Außerdem bei einer einzelnen Zahl")
+print("x^0 ergaenzen und bei einem einzelnen")
+print("x bitte eine 1 ergaenzen!")
+print("Desweiteren wird statt dem Komma")
+print("ein Punkt verwendet!")
+print("Ein Beispiel:")
+print("+3x^4.0 -5x^2.0 +1x^1.0 -9.5x^0.0")
+
+#Input
+func = input("f(x)= ")
+
+#Transformation
+func_bricks = list(func.split(" "))
+
+for i in range(0, len(func_bricks)):
+	func_bricks[i] = list(func_bricks[i].split("x^"))
+
+func_bricks.sort(key=lambda tup: tup[1], reverse=True)
+
+for i in range(0, len(func_bricks)):
+	for j in range(0, 2):
+		func_bricks[i][j] = float(func_bricks[i][j])
+
+print(func_bricks)
+
+#Run
+print(lib.pos_infinite(func_bricks))
+print(lib.neg_infinite(func_bricks))
+print(lib.derivative(func_bricks))
+print(lib.derivativeOfDerivative(func_bricks))
